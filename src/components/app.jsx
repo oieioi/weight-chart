ad073fe6b19ad3135057e931f0c42fd49c15ed24
oieioi <atsuinatsu.samuifuyu@gmail.com>
2015-04-08 03:19:45 +0900

    not working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,36 +3,38 @@ import React from 'react';
 
 export default React.createClass({
 
-  render() {
-alert(0);
-    var data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "My Second dataset",
-          fillColor: "rgba(151,187,205,0.2)",
-          strokeColor: "rgba(151,187,205,1)",
-          pointColor: "rgba(151,187,205,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
+  getInitialState(){
+    return {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: []
+          }
+        ]
+      }
     };
+  },
 
+  componentDidMount() {
+    this.state.data.labels.push('hoge');
+    this.state.data.datasets[0].data.push(2);
+    this.state.data.labels.push('fuga');
+    this.state.data.datasets[0].data.push(12);
+    this.state.data.labels.push('bar');
+    this.state.data.datasets[0].data.push(7);
+  },
+
+  render() {
     return (
-      <Line data={data} width="400" height="400" />
+      <Line data={this.state.data} width="400" height="400" redraw/>
     );
   }
 });
