89e1c19e687d660e0f370f8e900d529dcc1bceae
oieioi <atsuinatsu.samuifuyu@gmail.com>
2015-04-09 00:57:11 +0900

    Draw moving average chart (except `null`)

--- src/components/moving-average.jsx
+++ src/components/moving-average.jsx
@@ -3,11 +3,20 @@
 // ma = require('./moving-average');
 // ma([1,2,3,4,5,6,7,8,9,10], 3)
 
-export default function(arr, n){
+export default function ma(arr, n){
   return arr.map((item, index, ar)=> {
-    // 自分の値から n 個前までの値の平均を返す
-    var sliceFront = (index - n) > 0 ? (index -n ) : 0;
+    var sliceFront = index - n + 1;
+    var divisor = n;
+
+    if (sliceFront < 0) {
+      sliceFront = 0;
+      divisor = index + 1;
+    }
+
     var sum = ar.slice(sliceFront, index + 1).reduce((p, c)=> p + c);
-    return sum / n;
+    console.log(index, 'ar', sliceFront, '-', index + 1, ',sum:', sum, ', divisor:', divisor, 'average:', sum / divisor);
+    return sum / divisor;
   });
 }
+
+//console.log(ma([10, 12, 15, 20, 30, 20, 10, 12, 11], 4));
