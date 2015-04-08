01256ce3e7fcd053bd597dd553a4d2b0ebd7ce27
oieioi <atsuinatsu.samuifuyu@gmail.com>
2015-04-08 18:20:36 +0900

    Show the chart of the csv string in `textarea`

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,8 +5,9 @@ export default React.createClass({
 
   getInitialState(){
     return {
+      value: '',
       data: {
-        labels: ['item:1', 'item:2', 'item:3'],
+        labels: [],
         datasets: [
           {
             label: "My First dataset",
@@ -16,47 +17,89 @@ export default React.createClass({
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [1, 2, 3]
+            data: []
           }
         ]
       }
     };
   },
 
-  count: 4,
+  resetData() {
+    var newItem = this.state.data.datasets.slice();
+    newItem[0].data = [];
+    this.setState({
+      data:{
+        labels: [],
+        datasets: newItem
+      }
+    });
+  },
 
-  componentDidMount() {
-    setInterval(() => {
-      var newItem = this.state.data.datasets.slice();
-      var labels = this.state.data.labels.slice();
-      var rand = Math.floor(Math.random() * 20);
-      labels.push('item:' + this.count++);
-      newItem[0].data.push(rand);
+  setData({data, date}) {
 
-      this.setState({
-        data:{
-          labels: labels,
-          datasets: newItem
-        }
-      });
-    }, 2000);
+    if(!data || !date){
+      void 0;
+      // return;
+    }
+
+    var labels = this.state.data.labels;
+    labels.push(date);
+
+    var newItem = this.state.data.datasets;
+    newItem[0].data.push(+data || null);
+    console.log(newItem[0].data);
+
+    this.setState({
+      data:{
+        labels: labels,
+        datasets: newItem
+      }
+    });
 
   },
 
-  render() {
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    });
+  },
+
+  parseCsv(str, header){
+    var lines = str.split('\n');
+    return lines.map((ln) => {
+      var items = ln.split(',');
+      var ob = {};
+      header.forEach((it, count)=>ob[it] = items[count]);
+      return ob;
+    });
+  },
+
+  pushItem(){
+    var datas = this.parseCsv(this.state.value, ['date', 'data']);
+    datas.forEach((d)=>this.setData(d));
+  },
+
+  render(){
 
     var options = {
       animation: false
     };
 
+    var value = this.state.value;
+
     return (
-      <Line
-        data={this.state.data}
-        options={options}
-        width="400"
-        height="400"
-        redraw
-      />
+      <div>
+        <Line
+          data={this.state.data}
+          options={options}
+          width="800"
+          height="400"
+          redraw
+        />
+        <textarea value={value} onChange={this.handleChange} />
+        <button onClick={this.pushItem}>push</button>
+        <button onClick={this.resetData}>reset</button>
+      </div>
     );
   }
 });
